b7ec6754c39acb728570b8f1b450b59c0eb91db1
Chris Tyrrel <me@christyrrel.me>
2015-09-26 14:38:37 -0600

    Fix spacing.

--- lib/administration.rb
+++ lib/administration.rb
@@ -17,91 +17,94 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class Administration
-    include Cinch::Plugin
-	  match /[Dd][Ii][Ee]/, method: :die
-    match /[Ss][Aa][Yy] (#\S+) (.+)/, method: :say
-    match /[Aa][Cc][Tt] (#\S+) (.+)/, method: :act
-    match /[Jj][Oo][Ii][Nn] (#\S+)/, method: :join
-    match /[Pp][Aa][Rr][Tt] (#\S+)/, method: :part
-    match /[Nn][Ii][Cc][Kk] (.+)/, method: :nick
-    match /[Mm][Ss][Gg] (\S+) (.+)/, method: :msg
+  include Cinch::Plugin
+	match /[Dd][Ii][Ee]/, method: :die
+  match /[Ss][Aa][Yy] (#\S+) (.+)/, method: :say
+  match /[Aa][Cc][Tt] (#\S+) (.+)/, method: :act
+  match /[Jj][Oo][Ii][Nn] (#\S+)/, method: :join
+  match /[Pp][Aa][Rr][Tt] (#\S+)/, method: :part
+  match /[Nn][Ii][Cc][Kk] (.+)/, method: :nick
+  match /[Mm][Ss][Gg] (\S+) (.+)/, method: :msg
 
-    def die(m)
-        if permission(m)
-          $bots.each do |key, bot|
-          	puts "Terminating IRC connection for #{key}..."
-            adminchan(m, "#{Format(:red, "SHUTDOWN:")} #{Format(:orange, bot.irc.network.name.to_s)}")
-            bot.quit
-          end
-          $zncs.each do |key, bot|
-            puts "Terminating ZNC connection for #{key}..."
-            bot.quit
-          end
-        end
+  def die(m)
+    if permission(m)
+      $bots.each do |key, bot|
+        puts "Terminating IRC connection for #{key}..."
+        adminchan(m, "#{Format(:red, "SHUTDOWN:")} #{Format(:orange, bot.irc.network.name.to_s)}")
+        bot.quit
       end
+
+      $zncs.each do |key, bot|
+        puts "Terminating ZNC connection for #{key}..."
+        bot.quit
+      end
+    end
+  end
 
   def say(m, channel, message)
-      if permission(m) == true
-        @bot.channels.each do |channeln, blah|
-          if channeln == channel
-            Channel(channel).send (message)
-            return false
-          end
+    if permission(m) == true
+      @bot.channels.each do |channeln, blah|
+        if channeln == channel
+          Channel(channel).send (message)
+          return false
         end
+      end
+
       Target(m.user).notice "#{Format(:bold, @bot.nick)} is not on #{Format(:bold, channel)}."
-      end
     end
+  end
 
-    def act(m, channel, message)
-        if permission(m) == true
-          @bot.channels.each do |channeln, blah|
-            if channeln == channel
-              Channel(channel).action (message)
-              return false
-            end
-          end
-        Target(m.user).notice "#{Format(:bold, @bot.nick)} is not on #{Format(:bold, channel)}."
+  def act(m, channel, message)
+    if permission(m) == true
+      @bot.channels.each do |channeln, blah|
+        if channeln == channel
+          Channel(channel).action (message)
+          return false
         end
       end
 
-    def msg(m, target, msg)
-      if permission(m)
-        Target(target).send "#{msg}"
+      Target(m.user).notice "#{Format(:bold, @bot.nick)} is not on #{Format(:bold, channel)}."
+    end
+  end
+
+  def msg(m, target, msg)
+    if permission(m)
+      Target(target).send "#{msg}"
+    end
+  end
+
+  def nick(m, nicke)
+    if permission(m)
+      $bots.each do |key, bot|
+        bot.nick = nicke
       end
     end
+  end
 
-    def nick(m, nicke)
-        if permission(m)
-          $bots.each do |key, bot|
-            bot.nick = nicke
-          end
-        end
+  def join(m, chan)
+    if permission(m)
+      Channel(chan).join
     end
+  end
 
-    def join(m, chan)
-        if permission(m)
-          Channel(chan).join
-        end
-      end
-
-    def part(m, chan)
-      if permission(m)
-        Channel(chan).part
-      end
+  def part(m, chan)
+    if permission(m)
+      Channel(chan).part
     end
+  end
 
-	#Private Methods
-    def adminchan(m, message)
-      $adminbot.irc.send "PRIVMSG #{$config["adminchannel"]} :#{message}"
-    end
+	# Private Methods
+  def adminchan(m, message)
+    $adminbot.irc.send "PRIVMSG #{$config["adminchannel"]} :#{message}"
+  end
 
-    def permission(m)
-      if User(m.user).authname == $config["ownernickserv"]
-        return true
-      else
-        Target(m.user).notice "You do not have permission to run this command."
-        return false
-      end
+  def permission(m)
+    if User(m.user).authname == $config["ownernickserv"]
+      return true
+    else
+      Target(m.user).notice "You do not have permission to run this command."
+      return false
     end
+  end
 
 end
--- lib/channelmanagement.rb
+++ lib/channelmanagement.rb
@@ -17,144 +17,164 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class ChannelManagement
-    include Cinch::Plugin
-    match /topic (.+)/, method: :topic
-    match /kick (\S+) (.+)$/, method: :kick
-    match /kickban (\S+) (.+)$/, method: :kickban
-    match /ban (.+)/, method: :ban
-    match /unban (.+)/, method: :unban
-    match /voice (.+)/, method: :voice
-    match /devoice (.+)/, method: :devoice
+  include Cinch::Plugin
+  match /topic (.+)/, method: :topic
+  match /kick (\S+) (.+)$/, method: :kick
+  match /kickban (\S+) (.+)$/, method: :kickban
+  match /ban (.+)/, method: :ban
+  match /unban (.+)/, method: :unban
+  match /voice (.+)/, method: :voice
+  match /devoice (.+)/, method: :devoice
 
-    def topic(m, topic)
-      if permissions(m) == false
-        return false
-      end
-        if permissions(m)
-          Channel(m.channel).topic = topic
-        end
-      end
+  def topic(m, topic)
+    if permissions(m) == false
+      return false
+    end
+    if permissions(m)
+      Channel(m.channel).topic = topic
+    end
+  end
 
-    def kick(m, target, reason)
-      if permissions(m) == false
-        return false
-      end
-      if Channel(m.channel).has_user?(target) == false
-        Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
-        return false
-      end
-      Channel(m.channel).kick(target, reason + " (#{m.user.nick})")
-      Target(m.user).notice "Kicked #{Format(:bold, "#{target}")} from #{Format(:bold, "#{m.channel}")}."
+  def kick(m, target, reason)
+    if permissions(m) == false
+      return false
     end
 
-    def kickban(m, target, reason)
-      if permissions(m) == false
-        return false
-      end
-      if Channel(m.channel).has_user?(target) == false
-        Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
-        return false
-      end
-      Channel(m.channel).bans.each do |banmask, blah|
-        if User(target).match(banmask)
-          Channel(m.channel).kick(target, reason + " (#{m.user.nick})")
-          Target(m.user).notice "Kicked #{Format(:bold, "#{target}")} from #{Format(:bold, "#{m.channel}")}."
-        end
-        return false
-      end
-      Channel(m.channel).ban("*!*@"+User(target).host)
-      Channel(m.channel).kick(target, reason + " (#{m.user.nick})")
-      Target(m.user).notice "Kickbanned #{Format(:bold, "#{target}")} from #{Format(:bold, "#{m.channel}")}."
+    if Channel(m.channel).has_user?(target) == false
+      Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
+      return false
     end
 
-    def ban(m, target)
-      if permissions(m) == false
-        return false
-      end
-      if Channel(m.channel).has_user?(target) == false
-        Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
-        return false
-      end
-      bancount = 0
-      Channel(m.channel).bans.each do |banmask, blah|
-        if User(target).match(banmask)
-          bancount = bancount + 1
-        end
-      end
-      if bancount != 0
-        Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} found on #{Format(:bold, "#{m.channel}")}."
-        return false
-      end
-      Channel(m.channel).ban("*!*@"+User(target).host)
-      Target(m.user).notice "Banned #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
+    Channel(m.channel).kick(target, reason + " (#{m.user.nick})")
+    Target(m.user).notice "Kicked #{Format(:bold, "#{target}")} from #{Format(:bold, "#{m.channel}")}."
+  end
+
+  def kickban(m, target, reason)
+    if permissions(m) == false
+      return false
     end
 
-    def unban(m, target)
-      if permissions(m) == false
-        return false
+    if Channel(m.channel).has_user?(target) == false
+      Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
+      return false
+    end
+
+    Channel(m.channel).bans.each do |banmask, blah|
+      if User(target).match(banmask)
+        Channel(m.channel).kick(target, reason + " (#{m.user.nick})")
+        Target(m.user).notice "Kicked #{Format(:bold, "#{target}")} from #{Format(:bold, "#{m.channel}")}."
       end
-      if Channel(m.channel).has_user?(target) == false
-        if User(target).unknown?
-          Target(m.user).notice "There is no user #{Format(:bold, target)}."
-          return false
-        end
+      return false
+    end
+
+    Channel(m.channel).ban("*!*@"+User(target).host)
+    Channel(m.channel).kick(target, reason + " (#{m.user.nick})")
+    Target(m.user).notice "Kickbanned #{Format(:bold, "#{target}")} from #{Format(:bold, "#{m.channel}")}."
+  end
+
+  def ban(m, target)
+    if permissions(m) == false
+      return false
+    end
+
+    if Channel(m.channel).has_user?(target) == false
+      Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
+      return false
+    end
+
+    bancount = 0
+    Channel(m.channel).bans.each do |banmask, blah|
+      if User(target).match(banmask)
+        bancount = bancount + 1
       end
-      bancount = 0
-      Channel(m.channel).bans.each do |banmask, blah|
-        if User(target).match(banmask)
-          Channel(m.channel).unban(banmask)
-          bancount = bancount + 1
-        end
-      end
-      if bancount == 0
-        Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
-        return false
-      end
-      if bancount != 0
-        Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} removed on #{Format(:bold, "#{m.channel}")}."
+    end
+
+    if bancount != 0
+      Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} found on #{Format(:bold, "#{m.channel}")}."
+      return false
+    end
+
+    Channel(m.channel).ban("*!*@"+User(target).host)
+    Target(m.user).notice "Banned #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
+  end
+
+  def unban(m, target)
+    if permissions(m) == false
+      return false
+    end
+
+    if Channel(m.channel).has_user?(target) == false
+      if User(target).unknown?
+        Target(m.user).notice "There is no user #{Format(:bold, target)}."
         return false
       end
     end
 
-    def voice(m, target)
-      if permissions(m) == false
-        return false
-      end
-        if Channel(m.channel).has_user?(target) == false
-          Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
-          return false
-        end
-        if Channel(m.channel).voiced?(target)
-          Target(m.user).notice "#{Format(:bold, target)} is already voiced on #{Format(:bold, m.channel.name)}."
-          return false
-        end
-        Channel(m.channel).voice(target)
-        Target(m.user).notice "Voiced #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
-      end
-
-    def devoice(m, target)
-      if permissions(m) == false
-        return false
-      end
-      if Channel(m.channel).has_user?(target) == false
-        Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
-        return false
-      end
-      if Channel(m.channel).voiced?(target) == false
-        Target(m.user).notice "#{Format(:bold, target)} does not have voice on #{Format(:bold, m.channel.name)}."
-        return false
-      end
-      Channel(m.channel).devoice(target)
-      Target(m.user).notice "Devoiced #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
-    end
-
-#Helper Methods
-
-    def permissions(m)
-      if Channel(m.channel).opped?(m.user)
-        return true
-      else
-        return false
+    bancount = 0
+    Channel(m.channel).bans.each do |banmask, blah|
+      if User(target).match(banmask)
+        Channel(m.channel).unban(banmask)
+        bancount = bancount + 1
       end
     end
+
+    if bancount == 0
+      Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
+      return false
+    end
+
+    if bancount != 0
+      Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} removed on #{Format(:bold, "#{m.channel}")}."
+      return false
+    end
+  end
+
+  def voice(m, target)
+    if permissions(m) == false
+      return false
+    end
+
+    if Channel(m.channel).has_user?(target) == false
+      Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
+      return false
+    end
+
+    if Channel(m.channel).voiced?(target)
+      Target(m.user).notice "#{Format(:bold, target)} is already voiced on #{Format(:bold, m.channel.name)}."
+      return false
+    end
+
+    Channel(m.channel).voice(target)
+    Target(m.user).notice "Voiced #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
+  end
+
+  def devoice(m, target)
+    if permissions(m) == false
+      return false
+    end
+
+    if Channel(m.channel).has_user?(target) == false
+      Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
+      return false
+    end
+
+    if Channel(m.channel).voiced?(target) == false
+      Target(m.user).notice "#{Format(:bold, target)} does not have voice on #{Format(:bold, m.channel.name)}."
+      return false
+    end
+
+    Channel(m.channel).devoice(target)
+    Target(m.user).notice "Devoiced #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
+  end
+
+# Helper Methods
+
+  def permissions(m)
+    if Channel(m.channel).opped?(m.user)
+      return true
+    else
+      return false
+    end
+  end
+
 end
