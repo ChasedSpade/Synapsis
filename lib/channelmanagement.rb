470674b855db7bb5fc78acad5e82827f63b9a869
Chris Tyrrel <me@christyrrel.me>
2015-12-20 11:40:06 -0700

    Overhaul channel commands. Yes, Shnawlykins.. I saw the highlight.

--- lib/channelmanagement.rb
+++ lib/channelmanagement.rb
@@ -17,133 +17,93 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class ChannelManagement
-  include Cinch::Plugin
-  match /topic (.+)/, method: :topic
-  match /kick (\S+) (.+)$/, method: :kick
-  match /kickban (\S+) (.+)$/, method: :kickban
-  match /ban (.+)/, method: :ban
-  match /unban (.+)/, method: :unban
-  match /voice (.+)/, method: :voice
-  match /devoice (.+)/, method: :devoice
-
-  def topic(m, sTopic)
-    if Channel(m.channel).opped?(m.user)
-        Channel(m.channel).topic = sTopic
-    else
-        m.reply "You do not have permission to run this command."
-    end
-  end
-
-  def kick(m, target, reason)
-    if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
-        if Channel(m.channel).has_user?(target)
-            Channel(m.channel).kick(target, reason)
+    include Cinch::Plugin
+    match /topic (.+)/, method: :topic
+    match /kick (\S+) (.+)$/, method: :kick
+    match /kickban (\S+) (.+)$/, method: :kickban
+    match /ban (.+)/, method: :ban
+    match /unban (.+)/, method: :unban
+    match /voice (.+)/, method: :voice
+    match /devoice (.+)/, method: :devoice
+    
+    def topic(m, sTopic)
+        if Channel(m.channel).opped?(m.user)
+            Channel(m.channel).topic = sTopic
         else
-            Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
+            m.reply "You do not have permission to run this command."
         end
-    else
-        m.reply "You do not have permission to run this command."
     end
-  end
 
-  def kickban(m, target, reason)
-    if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
-        if Channel(m.channel).has_user?(target)
+    def kick(m, target, reason)
+        if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
+            if Channel(m.channel).has_user?(target)
+                Channel(m.channel).kick(target, reason)
+            else
+                Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
+            end
+        else
+            m.reply "You do not have permission to run this command."
+        end
+    end
+
+    def kickban(m, target, reason)
+        if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
+            if Channel(m.channel).has_user?(target)
+                Channel(m.channel).ban(target)
+                Channel(m.channel).kick(target, reason)
+            else
+                Target(m.user).notice("There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}")
+            end
+        else
+            m.reply "You do not have permission to run this command."
+        end
+    end
+
+    def ban(m, target)
+        if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
             Channel(m.channel).ban(target)
-            Channel(m.channel).kick(target, reason)
         else
-            Target(m.user).notice("There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}")
+            m.reply "You do not have permission to run this command."
         end
-    else
-        m.reply "You do not have permission to run this command."
-    end
-  end
-
-  def ban(m, target)
-    if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
-        Channel(m.channel).ban(target)
-    else
-        m.reply "You do not have permission to run this command."
-    end
-  end
-
-  def unban(m, target)
-    if permissions(m) == false
-      return false
     end
 
-    if Channel(m.channel).has_user?(target) == false
-      if User(target).unknown?
-        Target(m.user).notice "There is no user #{Format(:bold, target)}."
-        return false
-      end
+    def unban(m, target)
+        if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
+            Channel(m.channel).unban(target)
+        else
+            m.reply "You do not have permission to run this command."
+        end
     end
 
-    bancount = 0
-    Channel(m.channel).bans.each do |banmask, blah|
-      if User(target).match(banmask)
-        Channel(m.channel).unban(banmask)
-        bancount = bancount + 1
-      end
+    def voice(m, target)
+        if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
+            if Channel(m.channel).has_user?(target)
+                if Channel(m.channel).voiced?(target)
+                    Target(m.user).notice "#{Format(:bold, User(target).nick)} is already voiced."
+                else
+                    Channel(m.channel).voice(target)
+                end
+            else
+                m.reply "There is no user: #{Format(:bold, target)}"
+            end
+        else
+            m.reply "You do not have permission to run this command."
+        end
     end
 
-    if bancount == 0
-      Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
-      return false
+    def devoice(m, target)
+        if Channel(m.channel).opped?(m.user) && Channel(m.channel).opped?(@bot.nick)
+            if Channel(m.channel).has_user?(target)
+                if Channel(m.channel).voiced?(target)
+                    Channel(m.channel).devoice(target)
+                else
+                    m.reply "#{Format(:bold, User(target).nick)} is not voiced."
+                end
+            else
+                m.reply "There is no user: #{Format(:bold, target)}"
+            end
+        else
+            m.reply "You do not have permission to run this command."
+        end
     end
-
-    if bancount != 0
-      Target(m.user).notice "#{Format(:bold, "#{bancount}")} bans matching #{Format(:bold, "#{target}")} removed on #{Format(:bold, "#{m.channel}")}."
-      return false
-    end
-  end
-
-  def voice(m, target)
-    if permissions(m) == false
-      return false
-    end
-
-    if Channel(m.channel).has_user?(target) == false
-      Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
-      return false
-    end
-
-    if Channel(m.channel).voiced?(target)
-      Target(m.user).notice "#{Format(:bold, target)} is already voiced on #{Format(:bold, m.channel.name)}."
-      return false
-    end
-
-    Channel(m.channel).voice(target)
-    Target(m.user).notice "Voiced #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
-  end
-
-  def devoice(m, target)
-    if permissions(m) == false
-      return false
-    end
-
-    if Channel(m.channel).has_user?(target) == false
-      Target(m.user).notice "There is no user #{Format(:bold, target)} on #{Format(:bold, m.channel.name)}."
-      return false
-    end
-
-    if Channel(m.channel).voiced?(target) == false
-      Target(m.user).notice "#{Format(:bold, target)} does not have voice on #{Format(:bold, m.channel.name)}."
-      return false
-    end
-
-    Channel(m.channel).devoice(target)
-    Target(m.user).notice "Devoiced #{Format(:bold, "#{target}")} on #{Format(:bold, "#{m.channel}")}."
-  end
-
-# Helper Methods
-
-  def permissions(m)
-    if Channel(m.channel).opped?(m.user)
-      return true
-    else
-      return false
-    end
-  end
-
 end
